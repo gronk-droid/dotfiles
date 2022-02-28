9ce96084ec3c2120db6726c6da3ad55e0d2056dc
gronk-droid <grantbutler7@gmail.com>
2022-02-28 12:32:27 -0500

    reconfiguring sketchybarrc

--- .config/sketchybar/plugins/power.sh
+++ .config/sketchybar/plugins/power.sh
@@ -1,3 +1,29 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-sketchybar --set $NAME label=$(pmset -g batt)
+BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(pmset -g batt | grep 'AC Power')
+
+sketchybar --set ${NAME} icon.color=0xff2d2a2e
+
+if [[ ${CHARGING} != "" ]]; then
+  sketchybar --set ${NAME} icon=""
+  sketchybar --set ${NAME} label="${BATT_PERCENT}%"
+  exit 0
+fi
+
+case ${BATT_PERCENT} in
+    100) ICON="" ;;
+    9[0-9]) ICON="" ;;
+    8[0-9]) ICON="" ;;
+    7[0-9]) ICON="" ;;
+    6[0-9]) ICON="" ;;
+    5[0-9]) ICON="" ;;
+    4[0-9]) ICON="" ;;
+    3[0-9]) ICON="" ;;
+    2[0-9]) ICON="" ;;
+    1[0-9]) ICON="" ;;
+    *) ICON=""
+esac
+
+sketchybar --set ${NAME} icon="${ICON}"
+sketchybar --set ${NAME} label="${BATT_PERCENT}%"
