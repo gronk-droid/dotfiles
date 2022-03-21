d2195b25b0b2789a8f951aa66fc1c9e892bf0586
Grant Butler <grantbutler7@gmail.com>
2022-03-20 23:20:13 -0400

    update packages

--- .config/hyper/.hyper.js
+++ .config/hyper/.hyper.js
@@ -11,8 +11,7 @@ module.exports = {
     //   `project#1.0.1`
     plugins: [
         'hyper-solarized-light',
-        'hyper-font-ligatures',
-        'hyperminimal'
+        'hyper-font-ligatures'
     ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
