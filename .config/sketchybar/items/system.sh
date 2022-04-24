a691d1156043d12143a923a944b00965d2a6adc9
gronk-droid <grantbutler7@gmail.com>
2022-04-24 14:10:10 -0400

    update battery status script

--- .config/sketchybar/items/system.sh
+++ .config/sketchybar/items/system.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CHARGING=$(pmset -g batt | grep 'AC Power')
 if [[ ${CHARGING} != "" ]]; then
-    BATT_SCRIPT="$PLUGIN_DIR/charging.sh"
+    BATT_SCRIPT="charging.sh"
     BATT_ICON_SIZE="$FONT:Regular:26.0"
 else
-    BATT_SCRIPT="$PLUGIN_DIR/battery.sh"
+    BATT_SCRIPT="battery.sh"
     BATT_ICON_SIZE="$FONT:Regular:13.0"
 fi
 
@@ -20,11 +20,13 @@ sketchybar  --clone     system.label      label_template                        
                                                                                         \
             --add       item              battery left                                  \
             --set       battery           update_freq=100                               \
-                                          script="$BATT_SCRIPT"                         \
+                                          script="$PLUGIN_DIR/$BATT_SCRIPT"             \
                                           icon.font="$BATT_ICON_SIZE"                   \
                                           label.font="$FONT:Bold Italic:14.0"           \
                                           background.height=$SEGMENT_HEIGHT             \
                                           label.padding_right=0                         \
+            --subscribe battery           system_woke                                   \
+            --subscribe battery           space_change                                  \
                                                                                         \
             --add       item               system.caffeinate left                       \
             --set       system.caffeinate  update_freq=100                              \
