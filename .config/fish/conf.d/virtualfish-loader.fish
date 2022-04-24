d8e5563e5057fd562b8cf8e39b898ef70a9e92f9
gronk-droid <grantbutler7@gmail.com>
2022-04-24 14:06:15 -0400

    add vf functions

--- .config/fish/conf.d/virtualfish-loader.fish
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -3,4 +3,8 @@ set -g VIRTUALFISH_PYTHON_EXEC /usr/local/Cellar/python@3.9/3.9.10/bin/python3.9
 set -g VIRTUALFISH_HOME ~/.virtualenvs
 set -g WORKON_HOME ~/.virtualenvs
 source /usr/local/lib/python3.9/site-packages/virtualfish/virtual.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/compat_aliases.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/auto_activation.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/projects.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/environment.fish
 emit virtualfish_did_setup_plugins
