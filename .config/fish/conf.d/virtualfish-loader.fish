8c75784992a4f5db855ad01e3051deb7c9010c67
Grant Butler <grantbutler7@gmail.com>
2022-04-27 12:57:09 -0400

    new install with pipx

--- .config/fish/conf.d/virtualfish-loader.fish
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -1,10 +1,8 @@
 set -g VIRTUALFISH_VERSION 2.5.4
-set -g VIRTUALFISH_PYTHON_EXEC /usr/local/Cellar/python@3.9/3.9.10/bin/python3.9
-set -g VIRTUALFISH_HOME ~/.virtualenvs
-set -g WORKON_HOME ~/.virtualenvs
-source /usr/local/lib/python3.9/site-packages/virtualfish/virtual.fish
-source /usr/local/lib/python3.9/site-packages/virtualfish/compat_aliases.fish
-source /usr/local/lib/python3.9/site-packages/virtualfish/auto_activation.fish
-source /usr/local/lib/python3.9/site-packages/virtualfish/projects.fish
-source /usr/local/lib/python3.9/site-packages/virtualfish/environment.fish
+set -g VIRTUALFISH_PYTHON_EXEC /Users/gronk-droid/.local/pipx/venvs/virtualfish/bin/python
+source /Users/gronk-droid/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /Users/gronk-droid/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/compat_aliases.fish
+source /Users/gronk-droid/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+source /Users/gronk-droid/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/projects.fish
+source /Users/gronk-droid/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/environment.fish
 emit virtualfish_did_setup_plugins
